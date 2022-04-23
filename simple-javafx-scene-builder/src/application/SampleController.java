1208ff66af738da4f8ec7137d7b2fd991953ff6c
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2022-04-23 07:14:25 +0600

    add project with screen builder

---
+++ simple-javafx-scene-builder/src/application/SampleController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SampleController {
+	
+	   @FXML
+	    private void onClickBtn1(ActionEvent event) {
+	        System.out.println("Button click with screen builder !");
+	    }
+	
+}
